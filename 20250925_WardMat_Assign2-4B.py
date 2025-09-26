ce597984e369369fdbbd7c8b3cc328efbb4b1a31
Navi-tron <wolfpackfilmstudios@gmail.com>
2025-09-26 08:04:00 -0400

    Update 20250925_WardMat_Assign2-4B.py
    fixed more typos

--- 20250925_WardMat_Assign2-4B.py
+++ 20250925_WardMat_Assign2-4B.py
@@ -1,17 +1,17 @@
 # input statements
 salary = float(input('What is your salary? '))
-numDependents = float(input('How many dependants do you have? '))
+numDependents = float(input('How many dependents do you have? '))
 
 # calculate taxes here
 stateTax = float(salary * 0.065)
 federalTax = float(salary * .28)
-dependantDeduction = float(salary * (numDependents * .025))
-totalWithholding = float(stateTax + federalTax +dependantDeduction)
+dependentDeduction = float(salary * (numDependents * .025))
+totalWithholding = float(stateTax + federalTax + dependentDeduction)
 takeHomePay = salary - totalWithholding
 
 # output statements
 print('State Tax: $' + str(stateTax))
 print('Federal Tax: $' + str(federalTax))
-print('Dependant Deduction: $' + str(dependantDeduction))
+print('Dependent Deduction: $' + str(dependentDeduction))
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
