3181e65fa8ca9d9f9d141c5c5e158da2d40fc636
Navi-tron <wolfpackfilmstudios@gmail.com>
2025-09-26 08:02:29 -0400

    Update 20250925_WardMat_Assign2-4A.py
    fixed more typos

--- 20250925_WardMat_Assign2-4A.py
+++ 20250925_WardMat_Assign2-4A.py
@@ -5,13 +5,13 @@ numDependents = 2
 # calculate taxes here
 stateTax = float(salary * 0.065)
 federalTax = float(salary * .28)
-dependantDeduction = float(salary * (numDependents * .025))
-totalWithholding = float(stateTax + federalTax +dependantDeduction)
+dependentDeduction = float(salary * (numDependents * .025))
+totalWithholding = float(stateTax + federalTax + dependentDeduction)
 takeHomePay = salary - totalWithholding
 
 # output statements
 print('State Tax: $' + str(stateTax))
 print('Federal Tax: $' + str(federalTax))
-print('Dependant Deduction: $' + str(dependantDeduction))
+print('Dependent Deduction: $' + str(dependentDeduction))
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
